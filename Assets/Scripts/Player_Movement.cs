6cbc564dcb7f75d6aab50a08ac68b1e5bab34ad3
duduyiq2001 <86567562+duduyiq2001@users.noreply.github.com>
2024-01-22 00:22:03 -0800

    rotation1

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -9,6 +9,9 @@ public class Player_Movement : MonoBehaviour
     public float upwardForce = 500f;   // Force to move upwards
     public float downwardForce = 500f; // Force to move downwards
     [SerializeField] protected int MaxSpeed=200;
+    [SerializeField] protected float RotateAngle=200.0f;
+
+    [SerializeField] protected float RotationRecoverRate =2.0f;
     void Start()
     {
         // Ensure the Rigidbody is not kinematic
@@ -27,25 +30,44 @@ public class Player_Movement : MonoBehaviour
         }
 
         // Apply sideways force based on user input
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
         if (Input.GetKey("a"))
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+             if(rb.transform.rotation.eulerAngles.y >= -RotateAngle ){
+            rb.transform.Rotate(new Vector3(0.0f,-RotateAngle,0.0f));
+             }
+            
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+             if(rb.transform.rotation.eulerAngles.y <= RotateAngle ){
+            rb.transform.Rotate(new Vector3(0.0f,RotateAngle,0.0f));
+             }
         }
 
         // Add upward force when the "w" key is pressed
         if (Input.GetKey("w"))
         {
             rb.AddForce(0, upwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+             if(rb.transform.rotation.eulerAngles.x<= RotateAngle){
+            rb.transform.Rotate(new Vector3(RotateAngle,0.0f,0.0f));
+             }
         }
 
         // Add downward force when the "s" key is pressed
         if (Input.GetKey("s"))
         {
             rb.AddForce(0, -downwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+            if(rb.transform.rotation.eulerAngles.x>= -RotateAngle ){
+            rb.transform.Rotate(new Vector3(-RotateAngle,0.0f,0.0f));
+            }
+        }
+        if(!Input.anyKey){
+        //gradually return orientation
+        Vector3 currentAngle = new Vector3(rb.transform.rotation.x,rb.transform.rotation.y,rb.transform.rotation.z);
+
+        rb.transform.Rotate((new Vector3(0.0f,0.0f,0.0f)-currentAngle)*RotationRecoverRate); 
         }
 
         // Example check for falling below a certain level, can be modified or removed
