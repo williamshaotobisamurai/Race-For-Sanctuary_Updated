076e6dfa928b911b34e121d0b74cbb56189061f8
Leo <317389203@qq.com>
2025-01-23 10:50:56 +0800

    Update Laser

--- Assets/LWorkspace/ThirdParties/Hovl Studio/HSFiles/Scripts/Hovl_Laser.cs
+++ Assets/LWorkspace/ThirdParties/Hovl Studio/HSFiles/Scripts/Hovl_Laser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System;
 using UnityEngine;
+using Unity.Burst.CompilerServices;
 
 public class Hovl_Laser : MonoBehaviour
 {
@@ -12,98 +13,23 @@ public class Hovl_Laser : MonoBehaviour
     public bool useLaserRotation = false;
 
     public float MaxLength;
-    private LineRenderer Laser;
 
     public float MainTextureLength = 1f;
     public float NoiseTextureLength = 1f;
-    private Vector4 Length = new Vector4(1, 1, 1, 1);
-    private bool LaserSaver = false;
-    private bool UpdateSaver = false;
-
-    private ParticleSystem[] Effects;
-    private ParticleSystem[] Hit;
 
     [SerializeField] private LayerMask layerMask;
-
-    void Start()
-    {
-        Laser = GetComponent<LineRenderer>();
-        Effects = GetComponentsInChildren<ParticleSystem>();
-        Hit = HitEffect.GetComponentsInChildren<ParticleSystem>();
-    }
-
-    void Update()
-    {
-        Laser.material.SetTextureScale("_MainTex", new Vector2(Length[0], Length[1]));
-        Laser.material.SetTextureScale("_Noise", new Vector2(Length[2], Length[3]));
-        //To set LineRender position
-        if (Laser != null && UpdateSaver == false)
-        {
-            Laser.SetPosition(0, transform.position);
-            RaycastHit hit; //DELETE THIS IF YOU WANT USE LASERS IN 2D
-            //ADD THIS IF YOU WANNT TO USE LASERS IN 2D: RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.forward, MaxLength);       
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, MaxLength, layerMask))//CHANGE THIS IF YOU WANT TO USE LASERRS IN 2D: if (hit.collider != null)
-            {
-                //End laser position if collides with object
-                Laser.SetPosition(1, hit.point);
-
-                HitEffect.transform.position = hit.point + hit.normal * HitOffset;
-                if (useLaserRotation)
-                    HitEffect.transform.rotation = transform.rotation;
-                else
-                    HitEffect.transform.LookAt(hit.point + hit.normal);
-
-                foreach (var AllPs in Effects)
-                {
-                    if (!AllPs.isPlaying) AllPs.Play();
-                }
-                //Texture tiling
-                Length[0] = MainTextureLength * (Vector3.Distance(transform.position, hit.point));
-                Length[2] = NoiseTextureLength * (Vector3.Distance(transform.position, hit.point));
-
-                if (GameHelper.IsSkully(hit.collider, out Skully skully))
-                {
-                    if (IsReadyToHit())
-                    {
-                        HitSkully(skully);
-                    }
-                }
-            }
-            else
-            {
-                //End laser position if doesn't collide with object
-                var EndPos = transform.position + transform.forward * MaxLength;
-                Laser.SetPosition(1, EndPos);
-                HitEffect.transform.position = EndPos;
-                foreach (var AllPs in Hit)
-                {
-                    if (AllPs.isPlaying) AllPs.Stop();
-                }
-                //Texture tiling
-                Length[0] = MainTextureLength * (Vector3.Distance(transform.position, EndPos));
-                Length[2] = NoiseTextureLength * (Vector3.Distance(transform.position, EndPos));
-            }
-            if (Laser.enabled == false && LaserSaver == false)
-            {
-                LaserSaver = true;
-                Laser.enabled = true;
-            }
-        }
-    }
-
+ 
     public void DisablePrepare()
     {
-        if (Laser != null)
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (GameHelper.IsSkully(other, out Skully skully))
         {
-            Laser.enabled = false;
-        }
-        UpdateSaver = true;
-        //Effects can = null in multiply shooting
-        if (Effects != null)
-        {
-            foreach (var AllPs in Effects)
+            if (IsReadyToHit())
             {
-                if (AllPs.isPlaying) AllPs.Stop();
+                HitSkully(skully);
             }
         }
     }
@@ -121,5 +47,4 @@ public class Hovl_Laser : MonoBehaviour
     {
         return Time.time > lastHitTimestamp + hitInterval;
     }
-
 }
